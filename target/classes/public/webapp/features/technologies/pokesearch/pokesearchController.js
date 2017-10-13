2a2fe8921e11bbbbe613edfe56f017fe584b0118
Unknown <MorickClive@Gmail.com>
2017-10-13 14:29:32 +0100

    Push: Updates for Pokemon API
    Included some additional tweaks and tested the pokemon api to ensure working.

--- target/classes/public/webapp/features/technologies/pokesearch/pokesearchController.js
+++ target/classes/public/webapp/features/technologies/pokesearch/pokesearchController.js
@@ -4,6 +4,7 @@
         var vm = this;
         // holds a global set
         vm.myVar = undefined;
+        vm.loading = false;
         
         vm.reverse = false;
         vm.doSort = function()
@@ -15,10 +16,12 @@
 
         vm.printToConsole = function(pokemonNo){
             console.log("requesting data");
-
+            vm.loading = true;
+            
             pokeService.getPokemonList(pokemonNo).then(function(results){
                 console.log( results );
                 vm.myVar = results;
+                vm.loading = false;
             });
         }
     };
